5c977626d5c61d54db892d5c800a833ea89a5246
Yash Parihar <thakuryashparihar7089@gmail.com>
2023-09-11 22:32:50 +0530

    Header Layout changed

--- src/components/Layout/Header.jsx
+++ src/components/Layout/Header.jsx
@@ -33,7 +33,7 @@ const Header = () => {
 
   return (
     <>
-      <header>
+      {/* <header>
         <nav>
           <section className="top-nav container-fluid">
             <div
@@ -65,8 +65,152 @@ const Header = () => {
                 <li>
                   <Link to={"/contact"}>Contact</Link>
                 </li>
+
+                <li>
+                  {auth?.user ? (
+                    <img
+                      src={profile}
+                      alt=""
+                      className="user-profile"
+                      onClick={handleShow}
+                    />
+                  ) : (
+                    <button className={`btn register-btn text-white fs-6`}>
+                      <Link to={"/register"} className="text-white">
+                        Register
+                      </Link>
+                    </button>
+                  )}
+                </li>
               </ul>
             </div>
+
+            <Offcanvas show={show} onHide={handleClose} placement="end">
+              <Offcanvas.Header closeButton>
+                <Offcanvas.Title>
+                  <div className="sidebar-profile d-flex align-items-center gap-2">
+                    <img src={profile} alt="" className="user-profile" />
+
+                    <div className="profile-info p-0">
+                      <p className="profile-name m-0"> {auth?.user?.name} </p>
+                      <p className="profile-email m-0"> {auth?.user?.email} </p>
+                    </div>
+                  </div>
+                </Offcanvas.Title>
+              </Offcanvas.Header>
+              <Offcanvas.Body>
+                <div className="offCanvasBody">
+                  <ul>
+                    <li className="sidebar-list">
+                      <MDBIcon fas icon="user" />
+                      <Link to={"/dashboard/user/profile"}>Profile</Link>
+                    </li>
+                    <li className="sidebar-list">
+                      <MDBIcon fas icon="book" />
+                      <Link to={"/dashboard/user/courses"}>My Courses</Link>
+                    </li>
+                    <li className="sidebar-list">
+                      <MDBIcon fas icon="cog" />
+                      <Link to={"/dashboard/user/settings"}>Settings</Link>
+                    </li>
+                    <hr />
+                    <li>
+                      <button className="btn sidebar-btn fs-6">
+                        <Link
+                          to={"/login"}
+                          className="text-white d-flex flex-row align-items-center gap-2"
+                          onClick={handleLogout}
+                        >
+                          Sign Out <MDBIcon fas icon="sign-out-alt" />
+                        </Link>
+                      </button>
+                    </li>
+                  </ul>
+                </div>
+              </Offcanvas.Body>
+            </Offcanvas>
+          </section>
+        </nav>
+      </header> */}
+
+      <nav className="navbar navbar-expand-lg bg-body-tertiary">
+        <div className="container-fluid m-1">
+          <Link className="navbar-brand fs-2 fw-bold text-white" to={"/"}>
+            <img src={brandLogo} alt="" />
+            CodeNesters
+          </Link>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon" />
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item mx-1">
+                <Link to={"/"} className="nav-link active" aria-current="page">
+                  Home
+                </Link>
+              </li>
+              <li className="nav-item mx-1">
+                <Link
+                  to={"/about"}
+                  className="nav-link active"
+                  aria-current="page"
+                >
+                  About
+                </Link>
+              </li>
+              <li className="nav-item mx-1">
+                <Link
+                  to={"/contact"}
+                  className="nav-link active"
+                  aria-current="page"
+                >
+                  Contact
+                </Link>
+              </li>
+              <li className="nav-item dropdown">
+                <Link
+                  className="nav-link dropdown-toggle"
+                  role="button"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
+                  Careers
+                </Link>
+                <ul className="dropdown-menu">
+                  <li>
+                    <Link className="dropdown-item">Courses</Link>
+                  </li>
+                  <li>
+                    <Link className="dropdown-item">Blogs</Link>
+                  </li>
+                  {/* <li>
+                    <hr className="dropdown-divider" />
+                  </li>
+                  <li>
+                    <Link className="dropdown-item">Something else here</Link>
+                  </li> */}
+                </ul>
+              </li>
+            </ul>
+            {/* <form className="d-flex" role="search">
+              <input
+                className="form-control me-2"
+                type="search"
+                placeholder="Search"
+                aria-label="Search"
+              />
+              <button className="btn btn-outline-success" type="submit">
+                Search
+              </button>
+            </form> */}
 
             {auth?.user ? (
               <img
@@ -127,9 +271,9 @@ const Header = () => {
                 </div>
               </Offcanvas.Body>
             </Offcanvas>
-          </section>
-        </nav>
-      </header>
+          </div>
+        </div>
+      </nav>
     </>
   );
 };
