6f97de6e4a1e32554224a3a26eab856f1d0c212f
Yash Parihar <thakuryashparihar7089@gmail.com>
2023-11-03 18:49:13 +0530

    Used array map() for rendering founders cards

--- src/components/AboutComponents/Founders.jsx
+++ src/components/AboutComponents/Founders.jsx
@@ -2,12 +2,73 @@ import React from "react";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Carousel } from "react-responsive-carousel";
 import founder1 from "../../assets/images/FounderImage/founder1.jpeg";
+import img from "../../assets/images/FounderImage/founder-1.jpg";
 import { Link } from "react-router-dom";
-import linkedinIcon from "../../assets/images/FounderIcons/linkedin-icon.svg";
-import instagramIcon from "../../assets/images/FounderIcons/instagram-icon.svg";
-import githubIcon from "../../assets/images/FounderIcons/github-icon.svg";
+import { MDBIcon } from "mdb-react-ui-kit";
 
 const Founders = () => {
+  const founderCardData = [
+    {
+      card: {
+        name: "Yash Parihar",
+        designation: "Founder, CodeNesters",
+        about: ` Lorem ipsum dolor sit amet consectetur adipisicing elit.
+      Possimus cumque expedita dolorum provident velit ullam sunt,
+      quae veritatis doloremque vero libero voluptates dignissimos
+      similique reprehenderit corporis natus reiciendis? Ipsam,
+      perferendis. Lorem ipsum dolor sit amet consectetur,
+      adipisicing elit. Consequatur distinctio provident
+      voluptatibus facilis voluptates hic, facere eos sed soluta
+      dolorum incidunt, perspiciatis ipsa molestias adipisci minus
+      vero illum fugit repellat!`,
+        linkedin: "https://linkedin.com/in/bhagvendra-singh",
+        instagram: "https://instagram.com/asynchronous.1",
+        github: "https://github.com/Yashcodes",
+        image: founder1,
+      },
+    },
+
+    {
+      card: {
+        name: "Jalla Upendra Rao",
+        designation: "Founder, CodeNesters",
+        about: ` Lorem ipsum dolor sit amet consectetur adipisicing elit.
+      Possimus cumque expedita dolorum provident velit ullam sunt,
+      quae veritatis doloremque vero libero voluptates dignissimos
+      similique reprehenderit corporis natus reiciendis? Ipsam,
+      perferendis. Lorem ipsum dolor sit amet consectetur,
+      adipisicing elit. Consequatur distinctio provident
+      voluptatibus facilis voluptates hic, facere eos sed soluta
+      dolorum incidunt, perspiciatis ipsa molestias adipisci minus
+      vero illum fugit repellat!`,
+        linkedin: "https://linkedin.com/in/bhagvendra-singh",
+        instagram: "https://instagram.com/asynchronous.1",
+        github: "https://github.com/Yashcodes",
+        image: img,
+      },
+    },
+
+    {
+      card: {
+        name: "Yash Parihar",
+        designation: "Founder, CodeNesters",
+        about: ` Lorem ipsum dolor sit amet consectetur adipisicing elit.
+      Possimus cumque expedita dolorum provident velit ullam sunt,
+      quae veritatis doloremque vero libero voluptates dignissimos
+      similique reprehenderit corporis natus reiciendis? Ipsam,
+      perferendis. Lorem ipsum dolor sit amet consectetur,
+      adipisicing elit. Consequatur distinctio provident
+      voluptatibus facilis voluptates hic, facere eos sed soluta
+      dolorum incidunt, perspiciatis ipsa molestias adipisci minus
+      vero illum fugit repellat!`,
+        linkedin: "https://linkedin.com/in/bhagvendra-singh",
+        instagram: "https://instagram.com/asynchronous.1",
+        github: "https://github.com/Yashcodes",
+        image: founder1,
+      },
+    },
+  ];
+
   return (
     <div className="founder-section-bg p-5">
       <h4
@@ -36,179 +97,57 @@ const Founders = () => {
           emulateTouch={true}
           showIndicators={true}
         >
-          <div
-            className="card-container d-flex flex-row flex-wrap  align-items-start shadow mx-auto"
-            style={{ maxWidth: "1000px", borderRadius: "8px" }}
-          >
-            <div className="card-container-left" style={{ width: "380px" }}>
-              <img
-                className="img-responsive"
-                src={founder1}
-                alt=""
-                style={{
-                  maxWidth: "380px",
-                  height: "400px",
-                  borderRadius: "8px 0px 0px 8px",
-                }}
-              />
-            </div>
-
-            <div className="card-container-right" style={{ width: "615px" }}>
-              <div
-                className="right-card-content"
-                style={{ padding: "22px 26px 22px 12px" }}
-              >
-                <h2 className="m-0">Yash Parihar</h2>
-                <p className="fs-5 mb-2">Founder, CodeNesters</p>
-
-                <p
-                  className="founder-card-para"
+          {founderCardData.map((cardData) => (
+            <div
+              className="card-container d-flex flex-row flex-wrap  align-items-start shadow mx-auto"
+              style={{ maxWidth: "1000px", borderRadius: "8px" }}
+            >
+              <div className="card-container-left" style={{ width: "380px" }}>
+                <img
+                  className="img-responsive"
+                  src={cardData?.card?.image}
+                  alt=""
                   style={{
-                    textAlign: "justify",
-                    textJustify: "inter-word",
+                    maxWidth: "380px",
+                    height: "400px",
+                    borderRadius: "8px 0px 0px 8px",
                   }}
-                >
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Possimus cumque expedita dolorum provident velit ullam sunt,
-                  quae veritatis doloremque vero libero voluptates dignissimos
-                  similique reprehenderit corporis natus reiciendis? Ipsam,
-                  perferendis. Lorem ipsum dolor sit amet consectetur,
-                  adipisicing elit. Consequatur distinctio provident
-                  voluptatibus facilis voluptates hic, facere eos sed soluta
-                  dolorum incidunt, perspiciatis ipsa molestias adipisci minus
-                  vero illum fugit repellat!
-                </p>
+                />
+              </div>
 
-                <div className="founder-icons d-flex gap-2">
-                  <Link to={"https://linkedin.com/in/bhagvendra-singh"}>
-                    <img src={linkedinIcon} alt="" width={"44px"} />
-                  </Link>
-                  <Link to={"https://github.com/YashCodes"}>
-                    <img src={githubIcon} alt="" />
-                  </Link>
-                  <Link to={"https://instagram.com/asynchronous.1"}>
-                    <img src={instagramIcon} alt="" />
-                  </Link>
+              <div className="card-container-right" style={{ width: "615px" }}>
+                <div
+                  className="right-card-content"
+                  style={{ padding: "22px 26px 22px 12px" }}
+                >
+                  <h2 className="m-0">{cardData?.card?.name}</h2>
+                  <p className="fs-5 mb-2">{cardData?.card?.designation}</p>
+
+                  <p
+                    className="founder-card-para"
+                    style={{
+                      textAlign: "justify",
+                      textJustify: "inter-word",
+                    }}
+                  >
+                    {cardData?.card?.about}
+                  </p>
+
+                  <div className="founder-icons d-flex gap-2">
+                    <Link to={`${cardData?.card?.linkedin}`}>
+                      <MDBIcon fab icon="linkedin" />
+                    </Link>
+                    <Link to={`${cardData?.card?.github}`}>
+                      <MDBIcon fab icon="github" />
+                    </Link>
+                    <Link to={`${cardData?.card?.instagram}`}>
+                      <MDBIcon fab icon="instagram" />
+                    </Link>
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-
-          <div
-            className="card-container d-flex flex-row flex-wrap  align-items-start shadow mx-auto"
-            style={{ maxWidth: "1000px", borderRadius: "8px" }}
-          >
-            <div className="card-container-left" style={{ width: "380px" }}>
-              <img
-                className="img-responsive"
-                src={founder1}
-                alt=""
-                style={{
-                  maxWidth: "380px",
-                  height: "400px",
-                  borderRadius: "8px 0px 0px 8px",
-                }}
-              />
-            </div>
-
-            <div className="card-container-right" style={{ width: "615px" }}>
-              <div
-                className="right-card-content"
-                style={{ padding: "22px 26px 22px 12px" }}
-              >
-                <h2 className="m-0">Yash Parihar</h2>
-                <p className="fs-5 mb-2">Founder, CodeNesters</p>
-
-                <p
-                  className="founder-card-para"
-                  style={{
-                    textAlign: "justify",
-                    textJustify: "inter-word",
-                  }}
-                >
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Possimus cumque expedita dolorum provident velit ullam sunt,
-                  quae veritatis doloremque vero libero voluptates dignissimos
-                  similique reprehenderit corporis natus reiciendis? Ipsam,
-                  perferendis. Lorem ipsum dolor sit amet consectetur,
-                  adipisicing elit. Consequatur distinctio provident
-                  voluptatibus facilis voluptates hic, facere eos sed soluta
-                  dolorum incidunt, perspiciatis ipsa molestias adipisci minus
-                  vero illum fugit repellat!
-                </p>
-
-                <div className="founder-icons d-flex gap-2">
-                  <Link to={"https://linkedin.com/in/bhagvendra-singh"}>
-                    <img src={linkedinIcon} alt="" width={"44px"} />
-                  </Link>
-                  <Link to={"https://github.com/YashCodes"}>
-                    <img src={githubIcon} alt="" />
-                  </Link>
-                  <Link to={"https://instagram.com/asynchronous.1"}>
-                    <img src={instagramIcon} alt="" />
-                  </Link>
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <div
-            className="card-container d-flex flex-row flex-wrap  align-items-start shadow mx-auto"
-            style={{ maxWidth: "1000px", borderRadius: "8px" }}
-          >
-            <div className="card-container-left" style={{ width: "380px" }}>
-              <img
-                className="img-responsive"
-                src={founder1}
-                alt=""
-                style={{
-                  maxWidth: "380px",
-                  height: "400px",
-                  borderRadius: "8px 0px 0px 8px",
-                }}
-              />
-            </div>
-
-            <div className="card-container-right" style={{ width: "615px" }}>
-              <div
-                className="right-card-content"
-                style={{ padding: "22px 26px 22px 12px" }}
-              >
-                <h2 className="m-0">Yash Parihar</h2>
-                <p className="fs-5 mb-2">Founder, CodeNesters</p>
-
-                <p
-                  className="founder-card-para"
-                  style={{
-                    textAlign: "justify",
-                    textJustify: "inter-word",
-                  }}
-                >
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Possimus cumque expedita dolorum provident velit ullam sunt,
-                  quae veritatis doloremque vero libero voluptates dignissimos
-                  similique reprehenderit corporis natus reiciendis? Ipsam,
-                  perferendis. Lorem ipsum dolor sit amet consectetur,
-                  adipisicing elit. Consequatur distinctio provident
-                  voluptatibus facilis voluptates hic, facere eos sed soluta
-                  dolorum incidunt, perspiciatis ipsa molestias adipisci minus
-                  vero illum fugit repellat!
-                </p>
-
-                <div className="founder-icons d-flex gap-2">
-                  <Link to={"https://linkedin.com/in/bhagvendra-singh"}>
-                    <img src={linkedinIcon} alt="" width={"44px"} />
-                  </Link>
-                  <Link to={"https://github.com/YashCodes"}>
-                    <img src={githubIcon} alt="" />
-                  </Link>
-                  <Link to={"https://instagram.com/asynchronous.1"}>
-                    <img src={instagramIcon} alt="" />
-                  </Link>
-                </div>
-              </div>
-            </div>
-          </div>
+          ))}
         </Carousel>
       </div>
     </div>
