d11337d4897263da28e70e58f247f0991e288cb4
Yash Parihar <thakuryashparihar7089@gmail.com>
2024-01-20 19:50:41 +0530

    Footer alignment fixed

--- src/components/Layout/Footer.jsx
+++ src/components/Layout/Footer.jsx
@@ -9,100 +9,106 @@ const Footer = () => {
   return (
     <>
       <footer className="footer" id="footer">
-        <div className="container">
-          <div className="footer-content">
-            <div className="left-footer-content">
-              <div id="content-1">
-                <div className="footer-brand">
+        <div className="container footer-container">
+            <div className="footer-left" id="content-1">
+              <div className="footer-content-1">
+                <div className="footer-brand-header">
                   <img
                     src={brandLogo}
-                    alt=""
-                    className="brand-logo img-fluid"
+                    alt="logo"
+                    width={"44px"}
+                    height={"44px"}
                   />
-                  <span className="fw-bold fs-5">CODENESTERS</span>
+                  <span className="fw-bold">CODENESTERS</span>
                 </div>
-
-                <div className="footer-brand-slogan">
+                <div className="footer-brand-quote">
                   <span className="fw-bold">
                     Think | Code | Create | Deploy
                   </span>
-
+                </div>
+                <div className="content-1-para">
                   <p>
-                    Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                    Tenetur, pariatur Lorem ipsum dolor sit amet.
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Aperiam consequuntur veniam aliquam dicta, saepe voluptate?
                   </p>
                 </div>
               </div>
             </div>
 
-            <div className="mid-footer-content">
-              <div id="content-2 mx-2">
-                <div className="footer-content-head mb-3">
-                  <span className="fw-bold">MENU</span>
-                </div>
+            <div className="footer-right">
+              <div className="" id="content-2">
+                <div className="footer-content-2">
+                  <div className="content-2-head">
+                    <h6 className="fw-bold">MENU</h6>
+                  </div>
 
-                <div className="footer-content-list">
-                  <ul>
-                    <li>
-                      <Link to={"/"}> Home </Link>
-                    </li>
-                    <li>
-                      <Link to={"/about"}> About </Link>
-                    </li>
-                    <li>
-                      <Link to={"/contact"}> Contact </Link>
-                    </li>
-                  </ul>
+                  <div className="footer-content-list">
+                    <ul>
+                      <li>
+                        <Link to={"/"}>Home</Link>
+                      </li>
+                      <li>
+                        <Link to={"/about"}>About</Link>
+                      </li>
+                      <li>
+                        <Link to={"/contact"}>Contact</Link>
+                      </li>
+                    </ul>
+                  </div>
                 </div>
               </div>
 
-              <div id="content-3 mx-2">
-                <div className="footer-content-head mb-3">
-                  <span className="fw-bold">POLICIES</span>
-                </div>
+              <div className="" id="content-3">
+                <div className="footer-content-3">
+                  <div className="content-3-head">
+                    <h6 className="fw-bold">POLICIES</h6>
+                  </div>
 
-                <div className="footer-content-list">
-                  <ul>
-                    <li>
-                      <Link to={"/privacy-policy"}>Privacy Policy</Link>
-                    </li>
-                    <li>
-                      <Link to={"/terms-of-services"}>Terms of use</Link>
-                    </li>
-                    <li>
-                      <Link to={"/cancellation-and-refund"}>Refund & Cancellation Policy</Link>
-                    </li>
-                  </ul>
+                  <div className="footer-content-list">
+                    <ul>
+                      <li>
+                        <Link to={"/privacy-policy"}>Privacy Policy</Link>
+                      </li>
+                      <li>
+                        <Link to={"/terms-of-services"}>Terms of Use</Link>
+                      </li>
+                      <li>
+                        <Link to={"/cancellation-and-refund"}>
+                          Refund & Cancellation Policy
+                        </Link>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+
+              <div className="" id="content-4">
+                <div className="footer-content-3">
+                  <div className="content-4-head">
+                    <h6 className="fw-bold">GET IN TOUCH</h6>
+                  </div>
+
+                  <div className="footer-content-list">
+                    <ul>
+                      <li>
+                        <Link to={"tel:7722814150"}>
+                          <MDBIcon fas icon="phone" /> 7722814150
+                        </Link>
+                      </li>
+                      <li>
+                        <Link to={"mailto:codenesters3@gmail.com"}>
+                          <MDBIcon fas icon="envelope" /> codenesters3@
+                          gmail.com
+                        </Link>
+                      </li>
+                    </ul>
+                  </div>
                 </div>
               </div>
             </div>
-
-            <div className="right-footer-content">
-              <div id="content-4" className="fw-bold">
-                <div className="footer-content-head mb-3">
-                  <span className="fw-bold">GET IN TOUCH</span>
-                </div>
-
-                <div className="footer-content-list">
-                  <ul>
-                    <li className="d-flex align-items-center gap-1">
-                      <MDBIcon fas icon="phone" /> <span>77228 14150</span>
-                    </li>
-
-                    <li className="d-flex align-items-center gap-1">
-                      <MDBIcon fas icon="envelope" />{" "}
-                      <span className="p-0">
-                        codenesters3@ <span className="mx-0"> gmail.com</span>
-                      </span>
-                    </li>
-                  </ul>
-                </div>
-              </div>
-            </div>
-          </div>
         </div>
         <hr />
-        <span className="copyright text-center d-block">
+        <span className="copyright text-center d-block px-1">
           © Copyright CodeNesters. All Rights Reserved 2023
         </span>
       </footer>
