9e0b3aff59061d0b10e74403fc048dcee02ebabd
Yash Parihar <thakuryashparihar7089@gmail.com>
2024-01-22 23:33:36 +0530

    Added testimonial in Home by passing props to testimonial component

--- src/components/Testimonial.jsx
+++ src/components/Testimonial.jsx
@@ -1,98 +1,46 @@
 import React from "react";
-import client1 from "../assets/images/Anu/img11.jpg";
 import "../styles/Testimonial.css";
 
-const Testimonial = () => {
+const Testimonial = ({ HomeTestimonialData }) => {
   return (
     <>
       <section className="section-2" id="testimonial">
         <div className="container">
-          <h2
-            className="text-uppercase text-dark mb-3 text-center"
-           
-          >
+          <h2 className="text-uppercase text-dark mb-3 text-center">
             See what our clients say
           </h2>
-          <p className="testimonialHeadText" >
+          <p className="testimonialHeadText">
             Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cumque
             tempore temporibus <br />
             nemo mollitia esse, inventore magni quaerat.
           </p>
           <p className="border-btm" />
           <div className="row mb-5 testimonial-gap">
-            <div className="col-lg-4">
-              <div className="card">
-                <div className="face front-face">
-                  <img
-                    // src="https://images.unsplash.com/photo-1557862921-37829c790f19?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1051&q=80"
-                    src={client1}
-                    alt=""
-                    className="profile"
-                  />
-                  <div className="pt-3 text-uppercase name fw-bold">
-                    Anushka Baghel
+            {HomeTestimonialData.map((cardData) => (
+              <div className="col-lg-4" key={cardData?.card?.id}>
+                <div className="card">
+                  <div className="face front-face">
+                    <img
+                      // src="https://images.unsplash.com/photo-1557862921-37829c790f19?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1051&q=80"
+                      src={cardData?.card?.image}
+                      alt=""
+                      className="profile"
+                    />
+                    <div className="pt-3 text-uppercase name fw-bold">
+                      {cardData?.card?.title}
+                    </div>
+                    <div className="designation">
+                      {cardData?.card?.subTitle}
+                    </div>
                   </div>
-                  <div className="designation">Content Writer, Journalist</div>
-                </div>
-                <div className="face back-face">
-                  <span className="fas fa-quote-left" />
-                  <div className="testimonial">
-                    I made bacck the purchase price in just 48 hours! Thank you
-                    for making it pain less, pleasant. The service was
-                    execellent. I will refer everyone I know.
+                  <div className="face back-face">
+                    <span className="fas fa-quote-left" />
+                    <div className="testimonial">{cardData?.card?.content}</div>
+                    <span className="fas fa-quote-right" />
                   </div>
-                  <span className="fas fa-quote-right" />
-                </div>
-              </div>
-            </div>
-            <div className="col-lg-4">
-              <div className="card">
-                <div className="face front-face">
-                  <img
-                    src="https://images.unsplash.com/photo-1600486913747-55e5470d6f40?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80"
-                    alt=""
-                    className="profile"
-                  />
-                  <div className="pt-3 text-uppercase name fw-bold">
-                    Jeffery Kennan
-                  </div>
-                  <div className="designation">Full Stack Developer</div>
-                </div>
-                <div className="face back-face">
-                  <span className="fas fa-quote-left" />
-                  <div className="testimonial">
-                    Really good, you have saved our business! I made bacck the
-                    purchase price in just 48 hours! man, this thing is getting
-                    better and better as I learn more about it.
-                  </div>
-                  <span className="fas fa-quote-right" />
                 </div>
               </div>
-            </div>
-            <div className="col-lg-4">
-              <div className="card">
-                <div className="face front-face">
-                  <img
-                    src="https://images.unsplash.com/photo-1614574762522-6ac2fbba2208?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MjY2fHxtYW58ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
-                    alt=""
-                    className="profile"
-                  />
-                  <div className="pt-3 text-uppercase name fw-bold">
-                    Issac Maxwell
-                  </div>
-                  <div className="designation">Finance Director</div>
-                </div>
-                <div className="face back-face">
-                  <span className="fas fa-quote-left" />
-                  <div className="testimonial">
-                    Account keeper is the most valuable business research we
-                    have EVER purchased. Without electrician, we would ahave
-                    gone bankrupt by now.
-                  </div>
-                  <span className="fas fa-quote-right" />
-                </div>
-              </div>
-            </div>
+            ))}
           </div>
         </div>
       </section>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,6 +6,7 @@ import WhyUsSectionHome from "../components/WhyUsSectionHome";
 import BusinessProblemHome from "../components/BusinessProblemHome";
 import ContactFromHome from "../components/ContactFromHome";
 import { useLocation } from "react-router-dom";
+import client1 from "../assets/images/Anu/img11.jpg";
 
 const Home = () => {
   const location = useLocation();
@@ -15,6 +16,42 @@ const Home = () => {
       document.title = "Codenesters - Think | Code | Create | Deploy";
     }
   }, [location]);
+
+  //! Testimonial data
+  const HomeTestimonialData = [
+    {
+      card: {
+        id: 1,
+        title: "Anushka Baghel",
+        subTitle: "Content Writer, Journalist",
+        content:
+          "I made bacck the purchase price in just 48 hours! Thank you for making it pain less, pleasant. The service was execellent. I will refer everyone I know.",
+        image: client1,
+      },
+    },
+    {
+      card: {
+        id: 2,
+        title: "Jeffery Kennan",
+        subTitle: "Full Stack Developer",
+        content:
+          "I made bacck the purchase price in just 48 hours! Thank you for making it pain less, pleasant. The service was execellent. I will refer everyone I know.",
+        image:
+          "https://images.unsplash.com/photo-1600486913747-55e5470d6f40?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80",
+      },
+    },
+    {
+      card: {
+        id: 3,
+        title: "Issac Maxwell",
+        subTitle: "Finance Director",
+        content:
+          "I made bacck the purchase price in just 48 hours! Thank you for making it pain less, pleasant. The service was execellent. I will refer everyone I know.",
+        image:
+          "https://images.unsplash.com/photo-1614574762522-6ac2fbba2208?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MjY2fHxtYW58ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+      },
+    },
+  ];
 
   return (
     <Layout>
@@ -31,7 +68,7 @@ const Home = () => {
       <ContactFromHome />
 
       {/* //! Testimonial  */}
-      <Testimonial />
+      <Testimonial HomeTestimonialData={HomeTestimonialData} />
     </Layout>
   );
 };
