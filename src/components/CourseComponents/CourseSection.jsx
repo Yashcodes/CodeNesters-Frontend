3ac94665aeaf232742fbd13d369baf25e32ea9b4
Yash Parihar <thakuryashparihar7089@gmail.com>
2024-02-06 16:51:38 +0530

    added loader before loading the courses

--- src/components/CourseComponents/CourseSection.jsx
+++ src/components/CourseComponents/CourseSection.jsx
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 import CourseImage from "../../assets/images/contact/c1.jpg";
 import slugify from "slugify";
 import axios from "axios";
+import Loading from "../Utils/Loading";
 
 const CourseSection = () => {
   const [courses, setCourses] = useState([]);
@@ -33,65 +34,69 @@ const CourseSection = () => {
 
       <div className="course-cards container px-5">
         <div className="row d-flex flex-wrap flex-row my-5">
-          {courses?.map((course) => (
-            <div className="col-md-4 my-2" key={course?._id}>
-              <div className="course-card border shadow">
-                <div className="card-img course-card-img img-fluid">
-                  <img
-                    src={CourseImage}
-                    alt=""
-                    height={"250px"}
-                    width={"100%"}
-                  />
-                </div>
-
-                <div className="card-content p-3">
-                  <div className="course-card-head d-flex justify-content-between align-items-center">
-                    <h4
-                      className="text-black"
-                      style={{ fontSize: "22px", fontWeight: "600" }}
-                    >
-                      {course?.courseName}
-                    </h4>
-                    <span className="badge badge-success">
-                      {`${course?.coursePriceDiscount}% off`}
-                    </span>
+          {courses.length !== 0 ? (
+            courses?.map((course) => (
+              <div className="col-md-4 my-2" key={course?._id}>
+                <div className="course-card border shadow">
+                  <div className="card-img course-card-img img-fluid">
+                    <img
+                      src={CourseImage}
+                      alt=""
+                      height={"250px"}
+                      width={"100%"}
+                    />
                   </div>
-                  <p className="text-justify" style={{ fontSize: "16px" }}>
-                    {course?.courseContent}
-                  </p>
 
-                  <div className="course-card-btn d-flex align-items-center gap-2">
-                    <Link
-                      className="btn contact-banner-btn"
-                      to={`/courses/${slugify(
-                        course?._id,
-                        "-"
-                      ).toLowerCase()}`}
-                    >
-                      View Details
-                    </Link>
-
-                    <div className="card-price">
-                      <span className="m-0 text-gradient">
-                        Rs. {course?.courseDiscountedPrice}
-                        <p
-                          className="text-decoration-line-through d-inline"
-                          style={{
-                            marginLeft: "8px",
-                            fontWeight: "normal",
-                            color: "gray",
-                          }}
-                        >
-                          Rs. {course?.coursePrice}
-                        </p>
+                  <div className="card-content p-3">
+                    <div className="course-card-head d-flex justify-content-between align-items-center">
+                      <h4
+                        className="text-black"
+                        style={{ fontSize: "22px", fontWeight: "600" }}
+                      >
+                        {course?.courseName}
+                      </h4>
+                      <span className="badge badge-success">
+                        {`${course?.coursePriceDiscount}% off`}
                       </span>
+                    </div>
+                    <p className="text-justify" style={{ fontSize: "16px" }}>
+                      {course?.courseContent}
+                    </p>
+
+                    <div className="course-card-btn d-flex align-items-center gap-2">
+                      <Link
+                        className="btn contact-banner-btn"
+                        to={`/courses/${slugify(
+                          course?._id,
+                          "-"
+                        ).toLowerCase()}`}
+                      >
+                        View Details
+                      </Link>
+
+                      <div className="card-price">
+                        <span className="m-0 text-gradient">
+                          Rs. {course?.courseDiscountedPrice}
+                          <p
+                            className="text-decoration-line-through d-inline"
+                            style={{
+                              marginLeft: "8px",
+                              fontWeight: "normal",
+                              color: "gray",
+                            }}
+                          >
+                            Rs. {course?.coursePrice}
+                          </p>
+                        </span>
+                      </div>
                     </div>
                   </div>
                 </div>
               </div>
-            </div>
-          ))}
+            ))
+          ) : (
+            <Loading />
+          )}
         </div>
       </div>
     </div>
