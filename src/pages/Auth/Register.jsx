b14b929d57b8838e848321ea9978c5d7b09724fc
Yash Parihar <thakuryashparihar7089@gmail.com>
2024-02-09 23:34:27 +0530

    Added basic meta tags in register page using react-helmet | Some seo optimizations

--- src/pages/Auth/Register.jsx
+++ src/pages/Auth/Register.jsx
@@ -7,6 +7,7 @@ import axios from "axios";
 import { toast } from "react-hot-toast";
 import { useAuth } from "../../context/Auth";
 import SignIn from "../../assets/images/Register/signIn.svg";
+import { Helmet } from "react-helmet-async";
 // import { useFirebase } from "../../context/Firebase";
 
 const Register = () => {
@@ -17,16 +18,9 @@ const Register = () => {
 
   const [auth, setAuth] = useAuth();
 
-  const location = useLocation();
   const navigate = useNavigate();
 
   // const firebase = useFirebase();
-
-  useEffect(() => {
-    if (location.pathname === "/register") {
-      document.title = "Register - CodeNesters";
-    }
-  }, [location]);
 
   useEffect(() => {
     if (localStorage.getItem("auth")) {
@@ -85,109 +79,130 @@ const Register = () => {
   // }, [firebase, navigate]);
 
   return (
-    <div className="register-page container-fluid bg-gray-gradient">
-      <div className="register-left col-md-4 col-sm-9">
-        <h2 className="" color="darkgray" style={{ fontWeight: "600" }}>
-          Register to <span className="text-gradient">CodeNesters</span>
-        </h2>
-        <p className="fs-5">
-          Join our digital journey with high quality software services and
-          interactive learning guidance!
-        </p>
+    <>
+      <Helmet>
+        <title>Register - CodeNesters</title>
+        <meta
+          name="keywords"
+          content="Register, codenesters, register codenesters, register to codenesters, website development, web agency, web developer, register developer, sign up codenesters, create account codenesters, codenesters web services, codenesters website agency, don't have an account"
+        />
+        <meta
+          name="description"
+          content="Register to CodeNesters | Join our digital journey with high quality software services and interactive learning guidance!"
+        />
+        <link rel="canonical" href="https://www.codenesters.in/login" />
+      </Helmet>
 
-        <img src={SignIn} alt="" className="img-fluid" />
-      </div>
+      <div className="register-page container-fluid bg-gray-gradient">
+        <div className="register-left col-md-4 col-sm-9">
+          <h1 className="fs-2" color="darkgray" style={{ fontWeight: "600" }}>
+            Register to <span className="text-gradient">CodeNesters</span>
+          </h1>
+          <p className="fs-5">
+            Join our digital journey with high quality software services and
+            interactive learning guidance!
+          </p>
 
-      <div className="register-right col-md-4 col-sm-10">
-        <div className="register-card shadow-lg">
-          <div className="register-card-header my-3">
-            <div className="register-card-img">
-              <img src={Brand} alt="" width="34px" height="34px" />
+          <img
+            src={SignIn}
+            alt="Register CodeNesters"
+            className="img-fluid"
+            width={"auto"}
+            height={"auto"}
+          />
+        </div>
+
+        <div className="register-right col-md-4 col-sm-10">
+          <div className="register-card shadow-lg">
+            <div className="register-card-header my-3">
+              <div className="register-card-img">
+                <img src={Brand} alt="" width="34px" height="34px" />
+              </div>
+              <div className="card-head">
+                <h2>CodeNesters</h2>
+              </div>
             </div>
-            <div className="card-head">
-              <h2>CodeNesters</h2>
-            </div>
-          </div>
 
-          <div className="register-card-input">
-            <div className="register-name mb-3">
-              <MDBInput
-                label="Your Name"
-                type="text"
-                size="lg"
-                value={name}
-                onChange={(e) => setName(e.target.value)}
-                required
-              />
+            <div className="register-card-input">
+              <div className="register-name mb-3">
+                <MDBInput
+                  label="Your Name"
+                  type="text"
+                  size="lg"
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                  required
+                />
+              </div>
+              <div className="register-email mb-3">
+                <MDBInput
+                  label="Email Address"
+                  type="email"
+                  size="lg"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  required
+                />
+              </div>
+              <div className="register-password mb-3">
+                <MDBInput
+                  label="Enter Password"
+                  type="password"
+                  size="lg"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  required
+                />
+              </div>
+              <div className="register-password mb-3">
+                <MDBInput
+                  label="Confirm Password"
+                  type="password"
+                  size="lg"
+                  value={confirmPassword}
+                  onChange={(e) => setConfirmPassword(e.target.value)}
+                  required
+                />
+              </div>
             </div>
-            <div className="register-email mb-3">
-              <MDBInput
-                label="Email Address"
-                type="email"
-                size="lg"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                required
-              />
+
+            <div className="register-card-btn mb-2">
+              <MDBBtn
+                className={`w-100 card-btn`}
+                onClick={handleSubmit}
+                disabled={
+                  password !== confirmPassword ||
+                  name === "" ||
+                  email === "" ||
+                  password === "" ||
+                  confirmPassword === ""
+                }
+              >
+                Create Account
+              </MDBBtn>
             </div>
-            <div className="register-password mb-3">
-              <MDBInput
-                label="Enter Password"
-                type="password"
-                size="lg"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                required
-              />
+
+            <div className="divider-text text-center mb-2">or</div>
+
+            <div className="register-with-google mb-3">
+              <MDBBtn className="w-100 card-btn" onClick={""}>
+                SignUp with Google
+                <span>
+                  <MDBIcon fab icon="google" className="mx-2" size="lg" />
+                </span>
+              </MDBBtn>
             </div>
-            <div className="register-password mb-3">
-              <MDBInput
-                label="Confirm Password"
-                type="password"
-                size="lg"
-                value={confirmPassword}
-                onChange={(e) => setConfirmPassword(e.target.value)}
-                required
-              />
+
+            <div className="login-option mb-3">
+              <p className="text-center">
+                Do you already have an account?
+                <Link to={"/login"}> Login Here </Link>
+              </p>
             </div>
-          </div>
-
-          <div className="register-card-btn mb-2">
-            <MDBBtn
-              className={`w-100 card-btn`}
-              onClick={handleSubmit}
-              disabled={
-                password !== confirmPassword ||
-                name === "" ||
-                email === "" ||
-                password === "" ||
-                confirmPassword === ""
-              }
-            >
-              Create Account
-            </MDBBtn>
-          </div>
-
-          <div className="divider-text text-center mb-2">or</div>
-
-          <div className="register-with-google mb-3">
-            <MDBBtn className="w-100 card-btn" onClick={""}>
-              SignUp with Google
-              <span>
-                <MDBIcon fab icon="google" className="mx-2" size="lg" />
-              </span>
-            </MDBBtn>
-          </div>
-
-          <div className="login-option mb-3">
-            <p className="text-center">
-              Do you already have an account?
-              <Link to={"/login"}> Login Here </Link>
-            </p>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
