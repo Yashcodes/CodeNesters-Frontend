81ae8f33783939bfe80a596cba43c2e26cd23b8d
Yash Parihar <thakuryashparihar7089@gmail.com>
2024-02-09 12:01:33 +0530

    Removed unnecessary attribute fro hero section image

--- src/components/HeroSectionHome.jsx
+++ src/components/HeroSectionHome.jsx
@@ -22,11 +22,12 @@ const HeroSectionHome = () => {
             <p>Think | Code | Create | Deploy</p>
           </h2>
 
-          <p className="mb-0 text-justify" style={{fontSize : "19px"}}>
-            Creating the backbone of your online presence with visually
-            appealing and user-friendly interfaces at extremely low cost. Here,
-            we bring dreams to life through exceptional website and app
-            development and design services.
+          <p className="mb-0 text-justify" style={{ fontSize: "19px" }}>
+            The best software company in raipur engaged in creating the backbone
+            of your online presence with visually appealing and user-friendly
+            interfaces at extremely low cost. <br /> Here, we bring dreams to
+            life through exceptional website and app development and design
+            services.
           </p>
 
           <div className="hero-btn mb-3 mt-1">
@@ -51,7 +52,6 @@ const HeroSectionHome = () => {
             alt=""
             width={"370"}
             height={"200px"}
-            fetch
           />
         </div>
       </div>
