4fb48ad2d0694f72835fe0330f5d4260bb5ee3ad
Yash Parihar <thakuryashparihar7089@gmail.com>
2024-02-15 17:56:54 +0530

    Implemented darkmode in service banner

--- src/components/ServicesComponents/ServiceBanner.jsx
+++ src/components/ServicesComponents/ServiceBanner.jsx
@@ -1,17 +1,48 @@
 import React from "react";
 import serviceBannerImg from "../../assets/images/Services/service-banner.svg";
 import { Link } from "react-router-dom";
+import { useTheme } from "../../context/ThemeContext";
 
 const ServiceBanner = () => {
+  const { themeMode } = useTheme();
+
   return (
     <>
-      <div className="service-container container-fluid pt-5 px-2">
+      <div
+        className="service-container container-fluid pt-5 px-2"
+        style={
+          themeMode === "light"
+            ? {
+                backgroundImage:
+                  "linear-gradient(198deg, #bba9e2aa 0%, #ffffff 100%)",
+              }
+            : themeMode === "dark"
+            ? {
+                backgroundImage:
+                  "linear-gradient(451deg, rgb(157, 80, 187) -110%, rgb(0, 0, 0) 28%)",
+                color: "white",
+              }
+            : {
+                backgroundImage:
+                  "linear-gradient(198deg, #bba9e2aa 0%, #ffffff 100%)",
+              }
+        }
+      >
         <div className="service-banner-section d-flex align-items-center justify-content-center gap-5 flex-wrap">
           <div
             className="service-banner-left align-self-center d-flex flex-column"
             style={{ maxWidth: "700px" }}
           >
-            <p className="text-black fw-bold">
+            <p
+              className="fw-bold"
+              style={
+                themeMode === "light"
+                  ? { color: "rgba(9, 9, 9, 0.842)" }
+                  : themeMode === "dark"
+                  ? { color: "white" }
+                  : { color: "rgba(9, 9, 9, 0.842)" }
+              }
+            >
               We build websites that help your
               <span className="text-gradient"> Business</span> thrive and reach
               <span className="text-gradient"> Customers</span>
