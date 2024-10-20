eb4a44fe7a7f90cea7287aed5e0e4440ce5c6c0c
Yash Parihar <thakuryashparihar7089@gmail.com>
2024-10-20 11:18:13 +0530

    Added loader in courses section

--- src/components/CourseComponents/CourseSection.jsx
+++ src/components/CourseComponents/CourseSection.jsx
@@ -63,88 +63,92 @@ const CourseSection = ({ sectionHeading, headingContent, cardAnimation }) => {
         </h3>
 
         <div className="courseCardContainer">
-          {courses.map((course, index) => (
-            <div
-              className="courseCard"
-              data-aos={cardAnimation[index].aos}
-              style={
-                themeMode === "light"
-                  ? {
-                      boxShadow:
-                        "rgba(50, 50, 93, 0.25) 0px 13px 27px -5px, rgba(0, 0, 0, 0.3) 0px 8px 16px -8px",
-                    }
-                  : themeMode === "dark"
-                  ? {
-                      boxShadow:
-                        "rgb(72 46 95 / 25%) 0px 13px 54px 8px, rgb(67 12 117 / 55%) 0px 8px 14px 3px",
-                    }
-                  : {
-                      boxShadow:
-                        "rgba(50, 50, 93, 0.25) 0px 13px 27px -5px, rgba(0, 0, 0, 0.3) 0px 8px 16px -8px",
-                    }
-              }
-              key={course?._id}
-            >
-              <div className="">
-                <img
-                  className="rounded img-fluid"
-                  src={course?.courseImage}
-                  height={"150px"}
-                  alt=""
-                />
-              </div>
+          {courses.length !== 0 ? (
+            courses.map((course, index) => (
+              <div
+                className="courseCard"
+                data-aos={cardAnimation[index].aos}
+                style={
+                  themeMode === "light"
+                    ? {
+                        boxShadow:
+                          "rgba(50, 50, 93, 0.25) 0px 13px 27px -5px, rgba(0, 0, 0, 0.3) 0px 8px 16px -8px",
+                      }
+                    : themeMode === "dark"
+                    ? {
+                        boxShadow:
+                          "rgb(72 46 95 / 25%) 0px 13px 54px 8px, rgb(67 12 117 / 55%) 0px 8px 14px 3px",
+                      }
+                    : {
+                        boxShadow:
+                          "rgba(50, 50, 93, 0.25) 0px 13px 27px -5px, rgba(0, 0, 0, 0.3) 0px 8px 16px -8px",
+                      }
+                }
+                key={course?._id}
+              >
+                <div className="">
+                  <img
+                    className="rounded img-fluid"
+                    src={course?.courseImage}
+                    height={"150px"}
+                    alt=""
+                  />
+                </div>
 
-              <div className="card-head">
-                <h4>{course?.courseName}</h4>
-              </div>
+                <div className="card-head">
+                  <h4>{course?.courseName}</h4>
+                </div>
 
-              <div className="card-content">
-                <p
-                  style={
-                    themeMode === "light"
-                      ? {}
-                      : themeMode === "dark"
-                      ? {
-                          // backgroundImage:
-                          //   "radial-gradient(circle at 50% 50%, rgb(25 13 29) 0%, rgb(0 0 0) 115%)",
-                          color: "#bfbfbf",
-                        }
-                      : {}
-                  }
-                >
-                  {course?.courseContent}
-                </p>
-              </div>
+                <div className="card-content">
+                  <p
+                    style={
+                      themeMode === "light"
+                        ? {}
+                        : themeMode === "dark"
+                        ? {
+                            // backgroundImage:
+                            //   "radial-gradient(circle at 50% 50%, rgb(25 13 29) 0%, rgb(0 0 0) 115%)",
+                            color: "#bfbfbf",
+                          }
+                        : {}
+                    }
+                  >
+                    {course?.courseContent}
+                  </p>
+                </div>
 
-              <div className="course-card-btn d-flex align-items-center gap-2 justify-content-between mt-2">
-                      <Link
-                        className="btn contact-banner-btn"
-                        // to={`/courses/course/${slugify(
-                        //   course?._id,
-                        //   "-"
-                        // ).toLowerCase()}`}
+                <div className="course-card-btn d-flex align-items-center gap-2 justify-content-between mt-2">
+                  <Link
+                    className="btn contact-banner-btn"
+                    // to={`/courses/course/${slugify(
+                    //   course?._id,
+                    //   "-"
+                    // ).toLowerCase()}`}
+                  >
+                    Add to Cart
+                  </Link>
+
+                  <div className="card-price">
+                    <span className="m-0 text-gradient">
+                      Rs. {course?.coursePrice}
+                      <p
+                        className="text-decoration-line-through d-inline"
+                        style={{
+                          marginLeft: "8px",
+                          fontWeight: "normal",
+                          color: "gray",
+                        }}
                       >
-                        Add to Cart
-                      </Link>
-
-                      <div className="card-price">
-                        <span className="m-0 text-gradient">
-                          Rs. {course?.coursePrice}
-                          <p
-                            className="text-decoration-line-through d-inline"
-                            style={{
-                              marginLeft: "8px",
-                              fontWeight: "normal",
-                              color: "gray",
-                            }}
-                          >
-                            Rs. {course?.courseDiscountedPrice}
-                          </p>
-                        </span>
-                      </div>
-                    </div>
-            </div>
-          ))}
+                        Rs. {course?.courseDiscountedPrice}
+                      </p>
+                    </span>
+                  </div>
+                </div>
+              </div>
+            ))
+          ) : (
+            <Loading />
+          )}
         </div>
       </div>
     </section>
